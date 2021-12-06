0fe1dfa6ee278844b8c1a141f36bf32ac2dc49b6
Benito <benitocavazosr@yahoo.com>
2021-12-06 00:00:47 -0600

    quiz up to question 3

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var counter = 10;
+var counter = 75;
 
 var initCountdown = function(){
     
@@ -6,25 +6,7 @@ var initCountdown = function(){
         
         var countdown = function(){
             
-            var headherEl = document.querySelector("#question");
-            var introQuizEL =  document.createElement("h2");
-            introQuizEL.textContent = "Commonly used data types DO Not Include:";
-                
-
-            headherEl.appendChild(introQuizEL);
             
-            
-            var introButtonEl = document.querySelector("#buttons");
-            var startButtonEl = document.createElement("button");
-            startButtonEl.textContent = "1. Strings";
-            startButtonEl.className = "btn start-btn";
-            startButtonEl.setAttribute("data-answer-id", counter);
-
-            
-            introButtonEl.appendChild(startButtonEl);
-            startButtonEl.addEventListener("click", function(){
-                alert("button clicked");
-            });
             counter--;
             console.log(counter);
             if(counter === 0){
@@ -35,6 +17,292 @@ var initCountdown = function(){
     
 
     var startCountdown = setInterval(countdown, 1000);
+};
+
+
+var question1 = function(){
+    var counterEl = document.querySelector("#timer");
+
+    var timerEl = document.createElement("p");
+    timerEl.textContent = "Timer:" + counter;
+    timerEl.setAttribute("id" ,"data-timer1");
+    counterEl.appendChild(timerEl);
+
+    var headherEl = document.querySelector("#question");
+    var introQuizEL =  document.createElement("h2");
+    introQuizEL.textContent = "Commonly used data types DO Not Include:";
+    introQuizEL.setAttribute("id","data-header1");
+        
+
+    headherEl.appendChild(introQuizEL);
+    
+    
+    var introButtonEl = document.querySelector("#buttons");
+    var startButtonEl = document.createElement("button");
+    startButtonEl.textContent = "1. Strings";
+    startButtonEl.className = "btn start-btn";
+    startButtonEl.setAttribute("id", "btn-question1");
+
+    
+    introButtonEl.appendChild(startButtonEl);
+    startButtonEl.addEventListener("click", function(){
+        // alert("button clicked 1");
+        counter = counter - 10;
+        question2();
+
+        var answearEl = document.querySelector("#answear");
+
+        var responseEl = document.createElement("p");
+        responseEl.textContent = "Incorrect!";
+        responseEl.setAttribute("id" ,"data-answear");
+        answearEl.appendChild(responseEl);
+    });
+    
+    var introButtonEl = document.querySelector("#buttons");
+    var startButtonEl = document.createElement("button");
+    startButtonEl.textContent = "2. booleans";
+    startButtonEl.className = "btn start-btn";
+    startButtonEl.setAttribute("id", "btn-question2");
+
+    
+    introButtonEl.appendChild(startButtonEl);
+    startButtonEl.addEventListener("click", function(){
+        // alert("button clicked 2");
+        question2();
+        var answearEl = document.querySelector("#answear");
+
+        var responseEl = document.createElement("p");
+        responseEl.textContent = "Correct!";
+        responseEl.setAttribute("id" ,"data-answear");
+        answearEl.appendChild(responseEl);
+    });
+    
+    var introButtonEl = document.querySelector("#buttons");
+    var startButtonEl = document.createElement("button");
+    startButtonEl.textContent = "3. alerts";
+    startButtonEl.className = "btn start-btn";
+    startButtonEl.setAttribute("id", "btn-question3");
+
+    
+    introButtonEl.appendChild(startButtonEl);
+    startButtonEl.addEventListener("click", function(){
+        // alert("button clicked 3");
+        counter = counter - 10;
+        question2();
+        var answearEl = document.querySelector("#answear");
+
+        var responseEl = document.createElement("p");
+        responseEl.textContent = "Incorrect!";
+        responseEl.setAttribute("id" ,"data-answear");
+        answearEl.appendChild(responseEl);
+    });
+
+    var introButtonEl = document.querySelector("#buttons");
+    var startButtonEl = document.createElement("button");
+    startButtonEl.textContent = "4. numbers";
+    startButtonEl.className = "btn start-btn";
+    startButtonEl.setAttribute("id", "btn-question4");
+
+    
+    introButtonEl.appendChild(startButtonEl);
+    startButtonEl.addEventListener("click", function(){
+        // alert("button clicked 4");
+        counter = counter - 10;
+        question2();
+        var answearEl = document.querySelector("#answear");
+
+        var responseEl = document.createElement("p");
+        responseEl.textContent = "Incorrect!";
+        responseEl.setAttribute("id" ,"data-answear");
+        answearEl.appendChild(responseEl);
+    });
+
+
+;}
+
+var question2 = function(){
+    var elimHeader = document.getElementById("data-header1");
+    elimHeader.remove();
+    var elimP = document.getElementById("data-timer1");
+    elimP.remove();
+    var elimBtn = document.getElementById("btn-question1")
+    elimBtn.remove();
+    var elimBtn = document.getElementById("btn-question2")
+    elimBtn.remove();
+    var elimBtn = document.getElementById("btn-question3")
+    elimBtn.remove();
+    var elimBtn = document.getElementById("btn-question4")
+    elimBtn.remove();
+    // var elimP2 = document.getElementById("data-answear");
+    // elimP2.remove();
+
+    var counterEl = document.querySelector("#timer");
+
+    var timerEl = document.createElement("p");
+    timerEl.textContent = "Timer:" + counter;
+    timerEl.setAttribute("id" ,"data-timer1");
+    counterEl.appendChild(timerEl);
+
+    var headherEl = document.querySelector("#question");
+    var introQuizEL =  document.createElement("h2");
+    introQuizEL.textContent = "The condition in an if/else statemen is enclosed with __________";
+    introQuizEL.setAttribute("id","data-header1");
+        
+
+    headherEl.appendChild(introQuizEL);
+    
+    
+    var introButtonEl = document.querySelector("#buttons");
+    var startButtonEl = document.createElement("button");
+    startButtonEl.textContent = "1. quotes";
+    startButtonEl.className = "btn start-btn";
+    startButtonEl.setAttribute("id", "btn-question1");
+
+    
+    introButtonEl.appendChild(startButtonEl);
+    startButtonEl.addEventListener("click", function(){
+        // alert("button clicked 1");
+        counter = counter - 10;
+        question3();
+    });
+    
+    var introButtonEl = document.querySelector("#buttons");
+    var startButtonEl = document.createElement("button");
+    startButtonEl.textContent = "2. curly brackets";
+    startButtonEl.className = "btn start-btn";
+    startButtonEl.setAttribute("id", "btn-question2");
+
+    
+    introButtonEl.appendChild(startButtonEl);
+    startButtonEl.addEventListener("click", function(){
+        // alert("button clicked 2");
+        counter = counter - 10;
+        question3();
+    });
+    
+    var introButtonEl = document.querySelector("#buttons");
+    var startButtonEl = document.createElement("button");
+    startButtonEl.textContent = "3. parentesis";
+    startButtonEl.className = "btn start-btn";
+    startButtonEl.setAttribute("id", "btn-question3");
+
+    
+    introButtonEl.appendChild(startButtonEl);
+    startButtonEl.addEventListener("click", function(){
+        // alert("button clicked 3");
+        
+        question3();
+    });
+
+    var introButtonEl = document.querySelector("#buttons");
+    var startButtonEl = document.createElement("button");
+    startButtonEl.textContent = "4. square brackets";
+    startButtonEl.className = "btn start-btn";
+    startButtonEl.setAttribute("id", "btn-question4");
+
+    
+    introButtonEl.appendChild(startButtonEl);
+    startButtonEl.addEventListener("click", function(){
+        // alert("button clicked 4");
+        counter = counter - 10;
+        question3();
+    });
+
+
+
+
+
+
+};
+
+var question3 = function(){
+    var elimHeader = document.getElementById("data-header1");
+    elimHeader.remove();
+    var elimP = document.getElementById("data-timer1");
+    elimP.remove();
+    var elimBtn = document.getElementById("btn-question1")
+    elimBtn.remove();
+    var elimBtn = document.getElementById("btn-question2")
+    elimBtn.remove();
+    var elimBtn = document.getElementById("btn-question3")
+    elimBtn.remove();
+    var elimBtn = document.getElementById("btn-question4")
+    elimBtn.remove();
+
+    var counterEl = document.querySelector("#timer");
+
+    var timerEl = document.createElement("p");
+    timerEl.textContent = "Timer:" + counter;
+    timerEl.setAttribute("id" ,"data-timer1");
+    counterEl.appendChild(timerEl);
+
+    var headherEl = document.querySelector("#question");
+    var introQuizEL =  document.createElement("h2");
+    introQuizEL.textContent = "Arrays in Javascript can be used to store __________";
+    introQuizEL.setAttribute("id","data-header1");
+        
+
+    headherEl.appendChild(introQuizEL);
+    
+    
+    var introButtonEl = document.querySelector("#buttons");
+    var startButtonEl = document.createElement("button");
+    startButtonEl.textContent = "1. numberts and strings";
+    startButtonEl.className = "btn start-btn";
+    startButtonEl.setAttribute("id", "btn-question1");
+
+    
+    introButtonEl.appendChild(startButtonEl);
+    startButtonEl.addEventListener("click", function(){
+        // alert("button clicked 1");
+        counter = counter - 10;
+        question4();
+    });
+    
+    var introButtonEl = document.querySelector("#buttons");
+    var startButtonEl = document.createElement("button");
+    startButtonEl.textContent = "2. other arrays";
+    startButtonEl.className = "btn start-btn";
+    startButtonEl.setAttribute("id", "btn-question2");
+
+    
+    introButtonEl.appendChild(startButtonEl);
+    startButtonEl.addEventListener("click", function(){
+        // alert("button clicked 2");
+        counter = counter - 10;
+        question4();
+    });
+    
+    var introButtonEl = document.querySelector("#buttons");
+    var startButtonEl = document.createElement("button");
+    startButtonEl.textContent = "3. booleans";
+    startButtonEl.className = "btn start-btn";
+    startButtonEl.setAttribute("id", "btn-question3");
+
+    
+    introButtonEl.appendChild(startButtonEl);
+    startButtonEl.addEventListener("click", function(){
+        // alert("button clicked 3");
+        counter = counter - 10;
+        question4();
+    });
+
+    var introButtonEl = document.querySelector("#buttons");
+    var startButtonEl = document.createElement("button");
+    startButtonEl.textContent = "4. all of the above";
+    startButtonEl.className = "btn start-btn";
+    startButtonEl.setAttribute("id", "btn-question4");
+
+    
+    introButtonEl.appendChild(startButtonEl);
+    startButtonEl.addEventListener("click", function(){
+        // alert("button clicked 4");
+
+        question4();
+    });
+
+
+
 };
 
 // Function to start the Quiz
@@ -73,6 +341,7 @@ var Start = function(){
         
         
         initCountdown();
+        question1();
       });
 
 
